d16f8438fac9230b757056f4b7501d8c8044f825
Filip Fedorisin <fedorisinfilip@gmail.com>
2021-02-14 15:31:06 +0100

    ver 1.0.1

---
+++ components/LineChart.vue
@@ -0,0 +1,43 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    chartData: {
+      type: Array,
+      required: true,
+    },
+    options: {
+      type: Object,
+      required: true,
+    },
+  },
+  mounted() {
+    const dates = this.chartData
+      .slice()
+      .map((item) => item.date)
+      .reverse()
+    const cases = this.chartData
+      .slice()
+      .map((item) => item.cases)
+      .reverse()
+    this.renderChart(
+      {
+        labels: dates,
+        datasets: [
+          {
+            label: this.label,
+            data: cases,
+          },
+        ],
+      },
+      this.options
+    )
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,145 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">corona-slovakia</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <table class="regions">
+      <tr>
+        <th @click="sortRegions('region')">Kraj</th>
+        <th @click="sortRegions('totalInfected')">Celkovo nakazený</th>
+        <th @click="sortRegions('newInfected')">Nový Nakazený</th>
+      </tr>
+      <tr v-for="region in regions" :key="region.region">
+        <td>{{ region.region }}</td>
+        <td>{{ region.totalInfected }}</td>
+        <td>{{ region.newInfected }}</td>
+      </tr>
+    </table>
+    <table class="district">
+      <tr>
+        <th @click="sortDistricts('town')">Okres</th>
+        <th @click="sortDistricts('totalInfected')">Celkovo nakazený</th>
+        <th @click="sortDistricts('newInfected')">Nový Nakazený</th>
+      </tr>
+      <tr v-for="district in districts" :key="district.town">
+        <td>{{ district.town }}</td>
+        <td>{{ district.totalInfected }}</td>
+        <td>{{ district.newInfected }}</td>
+      </tr>
+    </table>
+    <line-chart class="lineChart" :chart-data="slovakiaByDate" :options="chartOptions" label="Prípady"></line-chart>
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+import LineChart from '~/components/LineChart.vue'
+
+export default {
+  name: 'Index',
+  components: {
+    LineChart,
+  },
+  async asyncData() {
+    const slovakia = await axios
+      .get('https://api.apify.com/v2/key-value-stores/GlTLAdXAuOz6bLAIO/records/LATEST?disableRedirect=true')
+      .then((res) => res.data)
+
+    const slovakiaByDate = await axios
+      .get('https://covid-193.p.rapidapi.com/history', {
+        params: {
+          country: 'slovakia',
+        },
+        headers: {
+          'x-rapidapi-key': '63b4e7aa5emsh31ee6582fe06880p1b48e5jsn89fb50032e03',
+          'x-rapidapi-host': 'covid-193.p.rapidapi.com',
+        },
+      })
+      .then((res) => {
+        const scrapped = []
+        res.data.response.forEach((item) => {
+          scrapped.push({
+            cases: item.cases.total,
+            date: item.day,
+          })
+        })
+        return scrapped
+      })
+
+    return { slovakia, slovakiaByDate }
+  },
+  data() {
+    return {
+      rawRegions: [],
+      rawDistricts: [],
+      regionsCurrentSort: 'region',
+      regionsSortDir: 'asc',
+      districtsCurrentSort: 'town',
+      districtsSortDir: 'asc',
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    }
+  },
+  computed: {
+    regions() {
+      return this.rawRegions.slice().sort((a, b) => {
+        let modifier = 1
+        if (this.regionsSortDir === 'desc') modifier = -1
+        if (a[this.regionsCurrentSort] < b[this.regionsCurrentSort]) return -1 * modifier
+        if (a[this.regionsCurrentSort] > b[this.regionsCurrentSort]) return 1 * modifier
+        return 0
+      })
+    },
+    districts() {
+      return this.rawDistricts.slice().sort((a, b) => {
+        let modifier = 1
+        if (this.districtsSortDir === 'desc') modifier = -1
+        if (a[this.districtsCurrentSort] < b[this.districtsCurrentSort]) return -1 * modifier
+        if (a[this.districtsCurrentSort] > b[this.districtsCurrentSort]) return 1 * modifier
+        return 0
+      })
+    },
+  },
+  created() {
+    this.rawRegions = this.slovakia.regionsData
+    this.rawDistricts = this.slovakia.districts
+  },
+  methods: {
+    sortRegions(s) {
+      if (s === this.regionsCurrentSort) {
+        this.regionsSortDir = this.regionsSortDir === 'asc' ? 'desc' : 'asc'
+      }
+      this.regionsCurrentSort = s
+    },
+    sortDistricts(s) {
+      if (s === this.districtsCurrentSort) {
+        this.districtsSortDir = this.districtsSortDir === 'asc' ? 'desc' : 'asc'
+      }
+      this.districtsCurrentSort = s
+    },
+  },
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
+  width: 100%;
+  margin: 0;
+  padding: 0;
   display: flex;
-  justify-content: center;
   align-items: center;
-  text-align: center;
-}
+  flex-flow: column wrap;
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  .regions {
+    min-width: 50%;
+    color: blue;
+  }
+  .district {
+    min-width: 50%;
+    color: red;
+  }
+  .lineChart {
+    min-width: 50%;
+    min-height: 500px;
+  }
 }
 </style>
